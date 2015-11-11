8899cb7be0d8a40b45f5a5832892f66669f76a48
romanlutz <romanlutz13@gmail.com>
2015-11-11 16:51:26 -0500

    - Updated README (tensorflow) - Started neural network prediction with dedicated python file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 from features import extract_features
-from svmprediction import compare_RBF_parameters
+from svm_prediction import compare_RBF_parameters
 
 random.seed(0)  # keep seed fixed for reproducibility
 
@@ -14,7 +14,7 @@ def main_brendan():
     compare_RBF_parameters(features, labels)
 
 def main_roman():
-    (features, labels, _, _) = extract_features(2014, 2014)
+    (features, labels, _, _) = extract_features(2013, 2014)
 
 def __main__():
     main_roman()
