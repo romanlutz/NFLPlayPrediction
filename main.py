c4e225c3367b8687a9f36d0675a4dfb00943911a
Roman Lutz <romanlutz13@gmail.com>
2015-11-11 16:32:28 -0500

    - Updated README - Reorganized main file such that all of us can make changes and try out things. Just change the __main__() method such that your own main method is executed. I copied the previous commands into both of your own methods such that whoever wrote it doesn't lose it!

--- main.py
+++ main.py
@@ -3,5 +3,20 @@ from features import extract_features
 from svmprediction import compare_RBF_parameters
 
 random.seed(0)  # keep seed fixed for reproducibility
-(features, labels, _, _) = extract_features(2014, 2014)
-compare_RBF_parameters(features,labels)
+
+
+def main_valentin():
+    (features, labels, _, _) = extract_features(2014, 2014)
+    compare_RBF_parameters(features, labels)
+
+def main_brendan():
+    (features, labels, _, _) = extract_features(2014, 2014)
+    compare_RBF_parameters(features, labels)
+
+def main_roman():
+    (features, labels, _, _) = extract_features(2014, 2014)
+
+def __main__():
+    main_roman()
+
+__main__()
--- svmprediction.py
+++ svmprediction.py
@@ -14,34 +14,37 @@ features = a (sample size) x (features) array containing the feature vectors of 
 true_labels = an array of the correct classification for each of the sample feature vectors
 kfold = the number of folds to use in the cross validation. Defaults to 5 fold if not specified.
 
-Returns (mean, standard deviation) of the provided eestimator's accuracy using kfold validation,
+Returns (mean, standard deviation) of the provided estimator's accuracy using kfold validation,
 and utilizes all available CPUs for the training and validation.
 '''
+
+
 def test_SVM(estimator, features, true_labels, kfold=5):
-	scores = cross_validation.cross_val_score(estimator, features, true_labels, cv=kfold, n_jobs=-2, verbose=1)
-	return scores.mean(), scores.std()
+    scores = cross_validation.cross_val_score(estimator, features, true_labels, cv=kfold, n_jobs=-2, verbose=1)
+    return scores.mean(), scores.std()
 
 
 def compare_RBF_parameters(features, true_labels):
-	vec = DictVectorizer()
-	vector = vec.fit_transform(features).toarray()
-	#Consider replacing the above with FeatureHasher for faster computation?
-	parameters =	{
-			'C': [pow(2,x) for x in range(-3,15,2)], #Possible error weights for the SVM.
-			'gamma': [pow(2,x) for x in range(-7,7,2)] #Possible gamma values for the SVM.	
-			}
-	search = GridSearchCV(SVC(), parameters, cv=3, n_jobs=-1, verbose=1)
-	search.fit(vector, true_labels)
-	print "Best Estimator:"
-	print search.best_estimator_
-	print
-	print "Parameters:"
-	print search.best_params_
-	print
-	print "Score:"
-	print search.best_score_
+    vec = DictVectorizer()
+    vector = vec.fit_transform(features).toarray()
+    # Consider replacing the above with FeatureHasher for faster computation?
+    parameters = {
+        'C': [pow(2, x) for x in range(-3, 15, 2)],  # Possible error weights for the SVM.
+        'gamma': [pow(2, x) for x in range(-7, 7, 2)]  # Possible gamma values for the SVM.
+    }
+    search = GridSearchCV(SVC(), parameters, cv=3, n_jobs=-1, verbose=1)
+    search.fit(vector, true_labels)
+    print "Best Estimator:"
+    print search.best_estimator_
+    print
+    print "Parameters:"
+    print search.best_params_
+    print
+    print "Score:"
+    print search.best_score_
 
-def plot_confusion_matrix(estimator,features,labels):
+
+def plot_confusion_matrix(estimator, features, labels):
     # Split the data into a training set and a test set
     X_train, X_test, y_train, y_test = train_test_split(features, labels, random_state=0)
 
@@ -50,15 +53,14 @@ def plot_confusion_matrix(estimator,features,labels):
 
     # Compute confusion matrix
     cm = confusion_matrix(y_test, y_pred)
-    recall = cm[1][0]  / (cm[0][0] + cm[1][0])
-    precision = cm[1][0]  / (cm[1][0] + cm[1][1])
+    recall = cm[1][0] / (cm[0][0] + cm[1][0])
+    precision = cm[1][0] / (cm[1][0] + cm[1][1])
     accuracy = (cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])
-      
 
     print(cm)
-    print "Recall:",recall*100,'%'
-    print "Precision:",precision*100,'%'
-    print "Accuracy:",accuracy*100,'%'
+    print "Recall:", recall * 100, '%'
+    print "Precision:", precision * 100, '%'
+    print "Accuracy:", accuracy * 100, '%'
 
     # Show confusion matrix in a separate window
     cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -68,5 +70,5 @@ def plot_confusion_matrix(estimator,features,labels):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
-    
+
     return cm
